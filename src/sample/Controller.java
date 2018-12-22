a85e4b6fd6d94acb13a66cc820ddefaef416ba26
Zokero <piotrekkukiela@gmail.com>
2018-12-22 16:45:46 +0100

    Add all buttons and fields, change the way of openig files
    Now you can open file after click open button. GUI looks like in c++ project

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,15 +1,29 @@
 package sample;
 
+import database.DatabaseForObjects;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.stage.FileChooser;
 
 import java.awt.*;
+import java.io.File;
 
 public class Controller {
     @FXML
     private TextField name;
+    @FXML
+    private Node border;
 
     public void onButtonClicked(){
         System.out.println("Hello "+ name);
     }
-
+    public void openFile(){
+                FileChooser fileChooser = new FileChooser();
+                fileChooser.setTitle("Open file");
+                File returnValue = fileChooser.showOpenDialog(border.getScene().getWindow());
+                String value = returnValue.toString();
+                DatabaseForObjects base = new DatabaseForObjects();
+                base.loadToDatabase(value);
+            }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,43 +1,22 @@
 package sample;
 
-import database.*;
+import database.DatabaseForObjects;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        //GridPane root = new GridPane();
-//        root.setAlignment(Pos.CENTER);
-//        root.setVgap(10);
-//        root.setHgap(10);
-//        Label greeting = new Label("Welcome to JavaFX");
-//        greeting.setTextFill(Color.RED);
-//        greeting.setFont(Font.font(("Times New Roman"),FontWeight.BOLD, 70));
-//        root.getChildren().add(greeting);
         primaryStage.setTitle("SMPD");
         primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
     }
 
-
     public static void main(String[] args) { launch(args);
-        DatabaseForObjects base = new DatabaseForObjects();
-            base.loadToDatabase("E:\\git-repos\\demo (1)\\SMPDjavaFX\\src\\database\\Maple_Oak.txt");
-
-
-
-
-
     }
-
-
-
 }
